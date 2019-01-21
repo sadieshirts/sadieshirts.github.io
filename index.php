f239bf755d6036f4dc59a1de38fb3875c6ac96c7
sadieshirts <sadieshirts@u.boisestate.edu>
2019-01-20 18:07:06 -0700

    Checking if my computer is talking to git

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-<head>
+<!-- <html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn Heroku - Hello World!</title>
+  </head>
+  <body>
+    <h1>Sadie Shirts!</h1>
+<style>
+h1 {
+  color: teal;
+  border: 3px teal solid;
+  font-size: 6em;
+  padding: 1em;
+  margin: 0.5em;
+  text-align: center;
+  font-family: sans-serif;
+}
+</style>
+  </body>
+</html> -->
+
+
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 body {font-family: Arial, Helvetica, sans-serif;}
@@ -93,3 +119,4 @@ span.psw {
 </form>
 
 </body>
+</html>
