936c02941bbd239b6c0f7b71681c42d2c2ffb55d
sadieshirts <sadieshirts@u.boisestate.edu>
2019-01-21 03:33:32 -0700

    DB is working

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -3,23 +3,19 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS dependents;
 
 -- Create an employees table.
--- CREATE TABLE employees (
---     em_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
---     em_first_name VARCHAR(50) NOT NULL,
---     em_last_name VARCHAR(50) NOT NULL,
--- );
-
-CREATE TABLE employees (
-	em_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `employees` (
+	em_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,	
     em_first_name VARCHAR(50) NOT NULL,
-    em_last_name VARCHAR(50) NOT NULL,
+    em_last_name VARCHAR(50) NOT NULL
 );
 
--- Create an employees table.
-CREATE TABLE dependents (
-    dep_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- Create a dependents table.
+CREATE TABLE `dependents` (
+    dep_id INT NOT NULL AUTO_INCREMENT,
     dep_first_name VARCHAR(50) NOT NULL,
     dep_last_name VARCHAR(50) NOT NULL,
+    em_id INT,
+    PRIMARY KEY (dep_id),
     FOREIGN KEY (em_id) REFERENCES employees(em_id)
 );
 
@@ -47,10 +43,14 @@ INSERT INTO employees (em_first_name, em_last_name) VALUES('Tesha', 'Danko');
 INSERT INTO employees (em_first_name, em_last_name) VALUES('Art', 'Lozada');
 
 -- Insert test data into dependents. 
--- INSERT INTO employees (em_first_name, em_last_name, em_id) VALUES('Leia', 'Benjamin', 1);
--- INSERT INTO employees (em_first_name, em_last_name, em_id) VALUES('Leia', 'Benjamin', 1);
--- INSERT INTO employees (em_first_name, em_last_name, em_id) VALUES('Leia', 'Benjamin', 1);
--- INSERT INTO employees (em_first_name, em_last_name, em_id) VALUES('Leia', 'Benjamin', 1);
+INSERT INTO dependents (dep_first_name, dep_last_name, em_id) VALUES('Leia', 'Benjamin', 1);
+INSERT INTO dependents (dep_first_name, dep_last_name, em_id) VALUES('Hariet', 'Benjamin', 1);
+INSERT INTO dependents (dep_first_name, dep_last_name, em_id) VALUES('Amy', 'Benjamin', 1);
+INSERT INTO dependents (dep_first_name, dep_last_name, em_id) VALUES('Liz', 'Carbonaro', 71);
+INSERT INTO dependents (dep_first_name, dep_last_name, em_id) VALUES('Al', 'Maliszewski', 181);
+INSERT INTO dependents (dep_first_name, dep_last_name, em_id) VALUES('Billy', 'Smith', 111);
+INSERT INTO dependents (dep_first_name, dep_last_name, em_id) VALUES('Butch', 'Charlotte', 191);
+
 
 
 
