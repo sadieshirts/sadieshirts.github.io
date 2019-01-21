464197333707ad8a0a99d18666fdd447ec0dc6a9
sadieshirts <sadieshirts@u.boisestate.edu>
2019-01-21 02:15:10 -0700

    Editing Dao

--- public/Dao.php
+++ public/Dao.php
@@ -33,4 +33,79 @@ class Dao
 		$conn = $this->getConnection();
 		return $conn->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
 	}
+
+		public function getEmployee($em_id)
+	{
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("SELECT * FROM employees WHERE em_id = :em_id");
+		$stmt->bindParam(":em_id", $em_id);
+		$stmt->execute();
+		return $stmt->fetch();
+	}
+
+	public function getBenefits($em_id)
+	{
+		$conn = $this->getConnection();
+		$numDependents = $this->getDependents($em_id);
+		$numADependents = $this->numADependents($em_id);
+		$isA = $this->isA($em_id);
+
+		$employeeBenefit = 1000;
+		$depBenefit = 500;
+		$aDiscount = 0.1;
+		$paycheck = 2000;
+		$numPaychecks = 26;
+
+
+		$totalCost = ($employeeBenefit - ($isA * $aDiscount * $employeeBenefit)) +
+						($numDependents * $depBenefit - ($numADependents * $aDiscount * $depBenefit));
+		return $totalCost;
+	}
+
+	public function getDependents($em_id) {
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("SELECT SUM(f_name) FROM dependents WHERE em_id = :em_id");
+		$stmt->bindParam(":em_id", $em_id);
+		$stmt->execute();
+		return $stmt->fetch();
+	}
+
+	public function getADependents($em_id) {
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("SELECT SUM(f_name) FROM dependents WHERE em_id = :em_id AND f_name LIKE 'a%'");
+		$stmt->bindParam(":em_id", $em_id);
+		$stmt->execute();
+		return $stmt->fetch();
+	}
+
+	public function isA($em_id){
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("SELECT em_id FROM employees WHERE f_name LIKE 'a%'
+								AND em_id = :em_id");
+		$stmt->bindParam(":em_id", $em_id);
+		$stmt->execute();
+
+		if(($em_id = $stmt->fetch())) {
+			// $digest = $user['password'];
+				return 1;
+		}
+		return 0;
+		// return $stmt->fetch();
+	}
+
+	public function validateUser($em_id){
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("SELECT * FROM employees WHERE em_id = :em_id");
+
+		$stmt->bindParam(':em_id', $em_id);
+		$stmt->execute();
+
+		if(($user = $stmt->fetch())) {
+			$digest = $user['password'];
+			if(password_verify($password, $digest)) {
+				return array('name' => $user['username'], 'id' => $user['user_id']);
+			}
+		}
+		return false;
+	}
 }
